f2117822fc7e67705af19a524b020fa050713f4d
Jeremy Nix <Jeremy.Nix@cchmc.org>
2020-10-14 07:01:04 -0400

    remove passthru resource script

--- custom-migrations/picsure/V4__CREATE_PASSTHROUGH_RESOURCE.sql
+++
@@ -1,4 +0,0 @@
-use picsure;
-
-INSERT INTO resource (uuid, targetURL, resourceRSPath, description, name, token)
-	VALUES (0x__PASSTHROUGH_RESOURCE_UUID__, NULL, 'http://hpds:8080/PIC-SURE-PASSTHRU/', 'HPDS PassThrough Resource', 'passthru', NULL);
